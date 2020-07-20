60ec9f3d849b7d71713e435a46bd3029fd96d7bd
Wietse Venema <wietsevenema@gmail.com>
2020-07-20 15:13:28 +0200

    Small changes after feedback

--- 3-compose/app/html/index.php
+++ 3-compose/app/html/index.php
@@ -1,29 +1,30 @@
 <?php
 require_once(__DIR__ . '/../vendor/autoload.php');
 
-## Warning: 
-##
-## This is just to demonstrate
-##  - Redis for sessions
-##  - Calling a service.
-##
-## I don't know what I am doing. 
-## Please write better PHP in production
+/*
+ * Warning: 
+ *
+ * This is just to demonstrate
+ *  - Redis for sessions
+ *  - Calling a service.
+ *
+ * I don't know what I am doing. 
+ * Please write better PHP in production
+ */
 
-
-# Set up session 
+// Set up session 
 ini_set("session.save_handler","redis");
 
 $redis_url = getenv('REDIS_URL', true);
 ini_set("session.save_path", "$redis_url?timeout=1&persistent=1");
 
 $session = session_start();
-# If you don't check this return you will get a (silent) warning.
+// If you don't check this return you will get a (silent) warning.
 if (!$session){ 
   throw new Exception("SESSION FAILED");
 }
 
-# Add to cart insecurely
+// Add to cart insecurely 
 if( isset($_GET['add']) ){
   $_SESSION['cart'] = isset($_SESSION['cart']) ? $_SESSION['cart'] : [];
   array_push($_SESSION['cart'], htmlspecialchars($_GET['add'], ENT_NOQUOTES));
@@ -37,23 +38,21 @@ if( isset($_GET['clear']) ){
   exit();
 }
 
-# Initialize template library
+// Initialize template library
 $loader = new \Twig\Loader\FilesystemLoader( __DIR__);
 $twig = new \Twig\Environment($loader, [
   'cache' => sys_get_temp_dir(),
   'debug' => getenv('DEBUG', true)
 ]);
 
-# Load products
-$client = new GuzzleHttp\Client();
+// Load products
+$client = new GuzzleHttp\Client(); // FIXME Add timeout (default is unlimited)
 $product_url = getenv('PRODUCTS_URL', true);
 $response = $client->get("$product_url/random");
-$products = json_decode($response->getBody(), JSON_THROW_ON_ERROR);
+$products = json_decode($response->getBody(), false, 512, JSON_THROW_ON_ERROR);
 
-# Render template
-$name = getenv('NAME', true);
+// Render template
 echo $twig->render('index.html', [
-  'name' => $name,
   'products' => $products,
   'cart' => $_SESSION['cart'],
 ]);
