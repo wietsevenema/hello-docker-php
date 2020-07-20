30f93245700834f67d7a86a0ec54fa685e440fc0
Wietse Venema <wietsevenema@gmail.com>
2020-07-20 13:54:20 +0200

    Add compose example

---
+++ 3-compose/app/html/index.php
@@ -0,0 +1,59 @@
+<?php
+require_once(__DIR__ . '/../vendor/autoload.php');
+
+## Warning: 
+##
+## This is just to demonstrate
+##  - Redis for sessions
+##  - Calling a service.
+##
+## I don't know what I am doing. 
+## Please write better PHP in production
+
+
+# Set up session 
+ini_set("session.save_handler","redis");
+
+$redis_url = getenv('REDIS_URL', true);
+ini_set("session.save_path", "$redis_url?timeout=1&persistent=1");
+
+$session = session_start();
+# If you don't check this return you will get a (silent) warning.
+if (!$session){ 
+  throw new Exception("SESSION FAILED");
+}
+
+# Add to cart insecurely
+if( isset($_GET['add']) ){
+  $_SESSION['cart'] = isset($_SESSION['cart']) ? $_SESSION['cart'] : [];
+  array_push($_SESSION['cart'], htmlspecialchars($_GET['add'], ENT_NOQUOTES));
+  header("Location: /");
+  exit();
+}
+
+if( isset($_GET['clear']) ){
+  $_SESSION['cart'] = [];
+  header("Location: /");
+  exit();
+}
+
+# Initialize template library
+$loader = new \Twig\Loader\FilesystemLoader( __DIR__);
+$twig = new \Twig\Environment($loader, [
+  'cache' => sys_get_temp_dir(),
+  'debug' => getenv('DEBUG', true)
+]);
+
+# Load products
+$client = new GuzzleHttp\Client();
+$product_url = getenv('PRODUCTS_URL', true);
+$response = $client->get("$product_url/random");
+$products = json_decode($response->getBody(), JSON_THROW_ON_ERROR);
+
+# Render template
+$name = getenv('NAME', true);
+echo $twig->render('index.html', [
+  'name' => $name,
+  'products' => $products,
+  'cart' => $_SESSION['cart'],
+]);
